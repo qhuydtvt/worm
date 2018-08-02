f565e1650550b0d0da492e97c4a38a613777960b
AnhQuann <anhquan19051905@gmail.com>
2018-08-02 16:51:24 +0700

    login css

--- login/views.py
+++ login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 def index(request):
-  return HttpResponse("Hello, world")
+  return render(request, 'index.html')
