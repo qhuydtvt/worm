8c6725f215d8fdea3a459bed773388632f9cb2c3
minhduc9699 <minhduc.096.99@gmail.com>
2018-08-30 13:06:06 +0700

    add teacher model to admin site

--- grade/admin.py
+++ grade/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Grade
+from .models import Grade, Teacher
 # Register your models here.
 
 admin.site.register(Grade)
+admin.site.register(Teacher)
