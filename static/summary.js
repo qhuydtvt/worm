88916d04376312df9b695106244a0553fc5f3037
AnhQuann <anhquan19051905@gmail.com>
2018-09-20 16:47:25 +0700

    .

--- static/summary.js
+++ static/summary.js
@@ -2,9 +2,9 @@ const context = {
   summaryTeachers: null,
   loading: false,
   incorrect: false,
-  totalDays: 0,
-  seconds: 0,
-  time: null,
+  // totalDays: 0,
+  // seconds: 0,
+  // time: null,
 }
 
 $(document).ready(() => {
@@ -44,21 +44,21 @@ const renderTeachers = (() => {
   $('#tbl_teacher_body').empty();
   teachers = context.summaryTeachers;
   teachers.forEach((teacher) => {
-    if (teacher.time === null) {
-      teacher.time = "0";
-      avgHour = 0;
-      context.time = "0";
-    } else {
-      convertTimeToSecond(teacher.time);
-      avgHour = context.seconds / context.totalDays;
-      convertSecondToTime(avgHour);
-    }
+    // if (teacher.time === null) {
+    //   teacher.time = "0";
+    //   avgHour = 0;
+    //   context.time = "0";
+    // } else {
+    //   convertTimeToSecond(teacher.time);
+    //   avgHour = context.seconds / context.totalDays;
+    //   convertSecondToTime(avgHour);
+    // }
     const tr = 
     $(`
       <tr id="${teacher._id}">
         <td>${teacher.lastName}</td>
-        <td>${teacher.time}</td>
-        <td>${context.time}</td>
+        <td>${teacher.time[0]}</td>
+        <td>${teacher.time[1]}</td>
       </tr>
     `).appendTo($('#tbl_teacher_body'));
   })
@@ -77,9 +77,9 @@ const fetchSummary = async (start_date, stop_date) => {
     setIncorrect(true);
   } else {
     setIncorrect(false);
-    if (res && res.teachers && res.total_days) {
+    if (res && res.teachers) {
       context.summaryTeachers = res.teachers;
-      context.totalDays = res.total_days;      
+      // context.totalDays = res.total_days;      
       renderTeachers();
     };
   };
@@ -104,16 +104,16 @@ const setIncorrect = (incorrect) => {
 }
 
 
-const convertTimeToSecond = (timeString) => {  
-  const timeSplit = timeString.split(':'); 
-  const seconds = (+timeSplit[0]) * 60 * 60 + (+timeSplit[1]) * 60 + (+timeSplit[2]); 
-  context.seconds = seconds;
-}
+// const convertTimeToSecond = (timeString) => {  
+//   const timeSplit = timeString.split(':'); 
+//   const seconds = (+timeSplit[0]) * 60 * 60 + (+timeSplit[1]) * 60 + (+timeSplit[2]); 
+//   context.seconds = seconds;
+// }
 
 
-const convertSecondToTime = (tick) => {
-  let hours = Math.floor(tick/3600);
-  let mins = Math.floor(tick/60);
-  let secs = tick % 60;
-  context.time =  hours + ":"  + mins + ":" + secs;
-}
+// const convertSecondToTime = (tick) => {
+//   let hours = Math.floor(tick/3600);
+//   let mins = Math.floor(tick/60);
+//   let secs = tick % 60;
+//   context.time =  hours + ":"  + mins + ":" + secs;
+// }
