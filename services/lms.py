9fb5b9e4e3e189872c73bae7304e11f72b32c555
AnhQuann <anhquan19051905@gmail.com>
2018-09-27 15:51:26 +0700

    fix bug

--- services/lms.py
+++ services/lms.py
@@ -1,8 +1,6 @@
 from tk_rest import TKRest
 
 
-api = TKRest("https://learn.techkids.vn/api")
+api = TKRest("https://learn.techkids.vn/api/")
 classroom = api.classrooms
 users = api.users
-users.url = users.url + '?listAll=1'
-classroom.url = classroom.url + '?listAll=1'
